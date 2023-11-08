44d340eed3c3baaa6f276c9c92d541405c74af46
Choo Hyun Ho <hyunho4178@gmail.com>
2023-11-09 01:51:43 +0900

    UI 기초작업

---
+++ lib/features/daily_news/presentation/pages/home/daily_news.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_bloc.dart';
+import 'package:news_app/features/daily_news/presentation/bloc/article/remote/remote_article_state.dart';
+
+import '../../../domain/entities/article.dart';
+
+class DailyNews extends StatelessWidget {
+  const DailyNews({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(context),
+      body: _buildBody(),
+    );
+  }
+
+  _buildAppbar(BuildContext context) {
+    return AppBar(
+      title: const Text(
+        'Daily News',
+        style: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  _buildBody() {
+    return BlocBuilder<RemoteArticlesBloc, RemoteArticlesState>(
+      builder: (_, state) {
+        return Container();
+      },
+    );
+  }
+}
